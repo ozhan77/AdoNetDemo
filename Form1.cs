8c8c478193e61c53f8463ddb1df803f09e7ddfcc
ogz <oguzhandky@gmail.com>
2022-04-25 09:57:28 +0300

    Inset

---
+++ Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdoNetDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        ProductDal _productDal = new ProductDal();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            dgwProducts.DataSource = _productDal.GetAll();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            _productDal.Add(new Product
+            {
+                Name = textBox1.Text,
+                UnitPrice =Convert.ToDecimal(textBox2.Text),
+                StockAmount =Convert.ToInt16(textBox3.Text)
+            });
+            dgwProducts.DataSource = _productDal.GetAll();
+        }
+    }
+}
---
+++ ProductDal.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdoNetDemo
+{
+    public class ProductDal
+    {
+        SqlConnection connection = new SqlConnection(@"server = (localdb)\mssqllocaldb;initial catalog=ECommerce;integrated security=true");
+        public List<Product> GetAll()
+        {
+            ConnectionControl();
+
+            SqlCommand command = new SqlCommand("select * from Products",connection);
+            SqlDataReader reader = command.ExecuteReader();
+            List < Product > products = new List<Product>();
+            while (reader.Read())
+            {
+                Product product = new Product
+                {
+                    Id = Convert.ToInt16(reader["Id"]),
+                    Name = reader["Name"].ToString(),
+                    UnitPrice = Convert.ToDecimal(reader["UnitPrice"]),
+                    StockAmount = Convert.ToInt16(reader["StockAmount"])
+                };
+                products.Add(product);
+            }
+            reader.Close();
+            connection.Close();
+            return products;
+        }
+        private void ConnectionControl()
+        {
+            if (connection.State == ConnectionState.Closed)
+            {
+                connection.Open();
+            }
+        }
+
+        public void Add(Product product)
+        {
+            ConnectionControl();
+            SqlCommand command = new SqlCommand("Insert into Products values(@name,@unitPrice,@stockAmount)", connection);
+            command.Parameters.AddWithValue("@name", product.Name);
+            command.Parameters.AddWithValue("@unitPrice", product.UnitPrice);
+            command.Parameters.AddWithValue("@stockAmount", product.StockAmount);
+
+            command.ExecuteNonQuery();
+            connection.Close();
+
+        }
+    }
+}
